e29fcca5223afad55f36ca97a012c177b8f5af73
Womsxd <45663319+Womsxd@users.noreply.github.com>
2021-06-09 19:19:29 +0800

    httpx改requests，解决云函数openssl版本太低导致的问题

--- request.py
+++ request.py
@@ -1,9 +1,9 @@
-import httpx
+import requests
 
 #这里实际上应该加个"-> dict"但是考虑到请求可能失败的关系，所以直接不声明返回变量
 def get(url:str, **headers:dict):
     try:
-        req = httpx.get(url, headers=headers)
+        req = requests.get(url, headers=headers)
         return req.json()
     except:
         print("请求失败，网络错误！")
@@ -11,7 +11,7 @@ def get(url:str, **headers:dict):
 
 def post(url:str, data:dict, **headers:dict):
     try:
-        req = httpx.post(url, data=data, headers=headers)
+        req = requests.post(url, data=data, headers=headers)
         return req.json()
     except:
         print("请求失败，网络错误！")
@@ -19,7 +19,7 @@ def post(url:str, data:dict, **headers:dict):
 
 def post_json(url:str, json, **headers:dict):
     try:
-        req = httpx.post(url, json=json, headers=headers)
+        req = requests.post(url, json=json, headers=headers)
         return req.json()
     except:
         print("请求失败，网络错误！")
